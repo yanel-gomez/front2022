2b05cb065d49f29d4d03154ac6a4310d73746836
Yanel <yanelgomez@tdf.edu.ar>
2022-08-01 21:41:42 -0300

    ejercicio leandro

---
+++ clase2907/2907.js
@@ -0,0 +1,23 @@
+// Agrega elementos a la lista
+
+let botonLista = document.getElementById("botonLista");
+let lista = document.getElementById("lista");
+let inputLista = document.getElementById("inputLista");
+
+function agregarElemento(){
+    let elemento = document.createElement("li");
+    elemento.innerHTML = inputLista.value;
+    lista.appendChild(elemento);
+}
+
+// Agrega párrafo al div
+
+let botonParrafo = document.getElementById("botonParrafo");
+
+function agregarParrafo(){
+    let divParrafo = document.getElementById("divParrafo");
+    let parrafo = document.createElement("p");
+    let inputParrafo = document.getElementById("inputParrafo");
+    parrafo.innerHTML = inputParrafo.value;
+    divParrafo.appendChild(parrafo);
+}
