c91939435b55f0cbe01cc704c8ff985c3da2a5c7
Yanel <yanelgomez@tdf.edu.ar>
2022-08-04 23:50:28 -0300

    corrección

--- clase2207/script.js
+++ clase2207/script.js
@@ -1,9 +1,5 @@
-let btn_nombre = document.getElementById("btn_nombre");
-let btn_apellido = document.getElementById("btn_apellido");
-let btn_edad = document.getElementById("btn_edad");
-btn_nombre.addEventListener("click", mostrar);
-btn_apellido.addEventListener("click", mostrar);
-btn_edad.addEventListener("click", mostrar);
+let btn_mostrar = document.getElementById("btn_mostrar");
+btn_mostrar.addEventListener("click", mostrar);
 
 function mostrar() {
     let nombre, apellido, edad = "n/n";
